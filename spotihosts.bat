1750daf31d0ccddada0b86735648a96413dc3b92
ruvelro <ruvelro@gmail.com>
2016-08-18 18:29:18 +0200

    Script for Windows
    Open as admin for add the hosts to the hosts file on WinDir

---
+++ spotihosts.bat
@@ -0,0 +1,45 @@
+@echo off
+
+set hostspath=%windir%\System32\drivers\etc\hosts
+
+echo #[Spotihosts v2.0] >> %hostspath%
+echo 0.0.0.0 adclick.g.doublecklick.net >> %hostspath%
+echo 0.0.0.0 adeventtracker.spotify.com >> %hostspath%
+echo 0.0.0.0 ads-fa.spotify.com >> %hostspath%
+echo 0.0.0.0 analytics.spotify.com >> %hostspath%
+echo 0.0.0.0 audio2.spotify.com >> %hostspath%
+echo 0.0.0.0 b.scorecardresearch.com >> %hostspath%
+echo 0.0.0.0 bounceexchange.com >> %hostspath%
+echo 0.0.0.0 bs.serving-sys.com >> %hostspath%
+echo 0.0.0.0 content.bitsontherun.com >> %hostspath%
+echo 0.0.0.0 core.insightexpressai.com >> %hostspath%
+echo 0.0.0.0 crashdump.spotify.com >> %hostspath%
+echo 0.0.0.0 d2gi7ultltnc2u.cloudfront.net >> %hostspath%
+echo 0.0.0.0 d3rt1990lpmkn.cloudfront.net >> %hostspath%
+echo 0.0.0.0 desktop.spotify.com >> %hostspath%
+echo 0.0.0.0 doubleclick.net >> %hostspath%
+echo 0.0.0.0 ds.serving-sys.com >> %hostspath%
+echo 0.0.0.0 googleadservices.com >> %hostspath%
+echo 0.0.0.0 googleads.g.doubleclick.net >> %hostspath%
+echo 0.0.0.0 gtssl2-ocsp.geotrust.com >> %hostspath%
+echo 0.0.0.0 log.spotify.com >> %hostspath%
+echo 0.0.0.0 media-match.com >> %hostspath%
+echo 0.0.0.0 omaze.com >> %hostspath%
+echo 0.0.0.0 open.spotify.com >> %hostspath%
+echo 0.0.0.0 pagead46.l.doubleclick.net >> %hostspath%
+echo 0.0.0.0 pagead2.googlesyndication.com >> %hostspath%
+echo 0.0.0.0 partner.googleadservices.com >> %hostspath%
+echo 0.0.0.0 pubads.g.doubleclick.net >> %hostspath%
+echo 0.0.0.0 redirector.gvt1.com >> %hostspath%
+echo 0.0.0.0 s0.2mdn.net >> %hostspath%
+echo 0.0.0.0 securepubads.g.doubleclick.net >> %hostspath%
+echo 0.0.0.0 spclient.wg.spotify.com >> %hostspath%
+echo 0.0.0.0 tpc.googlesyndication.com >> %hostspath%
+echo 0.0.0.0 v.jwpcdn.com >> %hostspath%
+echo 0.0.0.0 video-ad-stats.googlesyndication.com >> %hostspath%
+echo 0.0.0.0 weblb-wg.gslb.spotify.com >> %hostspath%
+echo 0.0.0.0 www.googleadservices.com >> %hostspath%
+echo 0.0.0.0 www.googletagservices.com >> %hostspath%
+echo 0.0.0.0 www.omaze.com >> %hostspath%
+
+exit
